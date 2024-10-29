7c7ebb57241633cea28f81101155623692c6e45f
hr <abdelhak.chenafa@univ-temcen.dz>
2024-10-29 23:01:18 +0100

    FIFOCaching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+FIFOcache module
+"""
+from base_caching import BaseCaching
+from collections import deque
+from typing import Any, Dict, Optional
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache class
+    """
+
+    def __init__(self):
+        """ Instantiation
+        """
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key: Any, item: Any) -> None:
+        """ Add an item in the cache using the FIFO mode.
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.queue.remove(key)
+
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard_key = self.queue.popleft()
+                del self.cache_data[discard_key]
+                print("DISCARD: {}".format(discard_key))
+
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key: Any) -> Optional[Dict[Any, Any]]:
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
