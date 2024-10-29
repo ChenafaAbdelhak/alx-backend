808747b21be46a30dcf99461aca32a729c9a81f8
hr <abdelhak.chenafa@univ-temcen.dz>
2024-10-29 22:39:46 +0100

    basicCache class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+BasicCache module
+"""
+from base_caching import BaseCaching
+from typing import Any, Dict, Optional
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class
+    """
+
+    def put(self, key: Any, item: Any) -> None:
+        """ Add an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key: Any) -> Optional[Dict[Any, Any]]:
+        """ Get an item by key.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        return self.cache_data.get(key)
