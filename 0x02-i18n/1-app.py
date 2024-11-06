2921c260448e7dd0e8cfae0bf26a959364a08ba3
hr <abdelhak.chenafa@univ-temcen.dz>
2024-11-06 01:12:19 +0100

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""basic babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """returns root html page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
